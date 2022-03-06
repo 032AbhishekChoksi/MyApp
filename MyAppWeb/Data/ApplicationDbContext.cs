a1a5981885801b81267198eb03da500e7dd6450e
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-03-06 22:48:54 +0530

    Add project files.

---
+++ MyAppWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
